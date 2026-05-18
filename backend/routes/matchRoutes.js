1f43fbc4e6be44210f37ced57e13a1c19293a03e
Shreya Maurya <you@example.com>
2026-05-18 14:13:20 +0530

    final rename

--- backend/routes/matchRoutes.js
+++ backend/routes/matchRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Candidate = require("../models/temp");
+const Candidate = require("../models/Candidate");
 
 router.post("/", async (req, res) => {
 
